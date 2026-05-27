c9f89ad8403492cd332832180f96fa468f9269cf
partmant <ghdckdgml191@gmail.com>
2026-05-28 07:55:30 +0900

    feat: 프로젝트 초기화 및 핵심 라이브러리 추가

---
+++ src/main/java/com/tilog/TilogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilogBackendApplication.class, args);
+	}
+
+}

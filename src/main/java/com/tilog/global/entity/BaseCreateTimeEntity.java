1fb874a207d2f4a2b4935916d4fe532e68490d0b
partmant <ghdckdgml191@gmail.com>
2026-06-02 11:05:35 +0900

    feat: 생성 시간 기록 전용 BaseCreateTimeEntity 추가

---
+++ src/main/java/com/tilog/global/entity/BaseCreateTimeEntity.java
@@ -0,0 +1,20 @@
+package com.tilog.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseCreateTimeEntity {
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}

3ad2f1cabe96e5dbfd1d23f14352a2158c32b024
partmant <ghdckdgml191@gmail.com>
2026-06-02 09:40:41 +0900

    refactor: CORS 설정 중복 제거

--- src/main/java/com/tilog/global/config/WebConfig.java
+++ src/main/java/com/tilog/global/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.tilog.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,16 +13,6 @@ import java.nio.file.Paths;
 public class WebConfig implements WebMvcConfigurer {
     @Value("${file.upload-path}")
     private String uploadPath;
-
-    // [FE---BE] CORS 허용 설정
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 
     // 업로드 이미지 정적 리소스 접근 경로 설정
     @Override
